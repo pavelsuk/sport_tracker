fe3b2485bd21736c21b5e8937a967729aaef3a1c
pavelsuk <pavelsuk@users.noreply.github.com>
2022-01-11 15:21:23 +0100

    Initial boilerplate for Activites

---
+++ client/activities.py
@@ -0,0 +1,16 @@
+import logging
+import logging.config
+import pathlib
+
+
+class Activities(object):
+    '''Data holder for activities.
+    The data can be read from CSV, stored directly to google sheet or to database through API
+    '''
+
+    def __init__(self) -> None:
+        super().__init__()
+
+
+if __name__ == "__main__":
+    activities = Activities()
